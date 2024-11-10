35a1c2f0fb35bc94b60dfdc11c88b70a29e71e11
tidozaza <114283580+txindvo@users.noreply.github.com>
2024-11-10 20:19:05 +0700

    Update Text.lua

--- src/Elements/Text.lua
+++ src/Elements/Text.lua
@@ -1,44 +1,21 @@
 local Root = script.Parent.Parent
+local Components = Root.Components
+local Flipper = require(Root.Packages.Flipper)
 local Creator = require(Root.Creator)
 
-local New = Creator.New
-local Components = Root.Components
+local Text = {}
+Text.__index = Text
+Text.__type = "Text"
 
-local Element = {}
-Element.__index = Element
-Element.__type = "Text"
-
-function Element:New(Config)
+function Text:New(Config)
 	assert(Config.Title, "Text - Missing Title")
-	Config.Callback = Config.Callback or function() end
+	Config.Content = Config.Content or ""
 
-	-- สร้าง TextFrame สำหรับแสดงข้อความ
-	local TextFrame = require(Components.Element)(Config.Title, Config.Description, self.Container, false)
+	local Text = require(Components.Element)(Config.Title, Config.Content, Text.Container, false)
+	Text.Frame.BackgroundTransparency = 0.92
+	Text.Border.Transparency = 0.6
 
-	-- สร้าง TextLabel และวางไว้ที่มุมบนซ้ายของ TextFrame
-	local TextLabel = New("TextLabel", {
-		Text = Config.Title,
-		Size = UDim2.new(1, -10, 0, 20), -- ขนาดของ TextLabel (ความกว้างเต็มกรอบ ยกเว้นระยะขอบ 10)
-		AnchorPoint = Vector2.new(0, 0), -- จัดแนวที่มุมบนซ้าย
-		Position = UDim2.new(0, 10, 0, 10), -- ระยะขอบซ้าย 10 และบน 10
-		BackgroundTransparency = 1,
-		TextColor3 = Color3.fromRGB(255, 255, 255),
-		TextXAlignment = Enum.TextXAlignment.Left,
-		TextYAlignment = Enum.TextYAlignment.Top,
-		Parent = TextFrame.Frame,
-		ThemeTag = {
-			TextColor3 = "Text",
-		},
-	})
-
-	-- หากต้องการให้มี Callback เมื่อผู้ใช้คลิกที่ข้อความ
-	TextLabel.InputBegan:Connect(function(input)
-		if input.UserInputType == Enum.UserInputType.MouseButton1 then
-			self.Library:SafeCallback(Config.Callback)
-		end
-	end)
-
-	return TextFrame
+	return Paragraph
 end
 
-return Element
+return Paragraph
